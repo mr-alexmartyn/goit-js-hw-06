625e7390b8279ef07680e6fd4f5810660ecbf4c0
mr-alexmartyn <111350218+mr-alexmartyn@users.noreply.github.com>
2023-01-16 22:38:24 +0200

    Final tasks

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const logForm = document.querySelector(".login-form");
+logForm.addEventListener("submit", onLogSubmit);
 
+function onLogSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Поле не может оставаться пустым");
+  }
+  console.log(`Email:  ${email.value}`);
+  console.log(`Password: ${password.value}`);
+  event.currentTarget.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonChange = document.querySelector("button.change-color");
+const colorSpan = document.querySelector("span.color");
+const body = document.querySelector("body");
+
+buttonChange.addEventListener("click", () => {
+  let fillColor = getRandomHexColor();
+  colorSpan.textContent = fillColor;
+  body.style.backgroundColor = fillColor;
+});
